91ffe0f1f0ecce06a227eecb15325784ff5ede70
Daniel Jackoway <danjdel@gmail.com>
2010-11-11 07:40:49 -0600

    switched over to using MAGNETIC_FIELD for now. not sure why accelerometer isn't working

--- assets/scripts/accelerate_activity.rb
+++ assets/scripts/accelerate_activity.rb
@@ -17,23 +17,23 @@ $activity.when_launched do |bundle|
     @accelerometer = accelerometers[0]
   end
 
-
   setup_content do
     text_view :text => "shake!"
   end
 
-  handle_sensor_changed do |sensor, vals|
-    if sensor.getType == Sensor::TYPE_ACCELEROMETER
-      Log.v "SENSOR", "accel"
+  handle_sensor_changed do |sensor_event|
+    if sensor_event.sensor.getType == Sensor::TYPE_MAGNETIC_FIELD
       getWindow.setBackgroundDrawable ColorDrawable.new(Color.rgb(rand(255), rand(255), rand(255)))
     end
   end
 
   handle_pause do
+    Log.v "SENSORREG", "unregistered accel"
     @sensors.unregisterListener self, @accelerometer
   end
 
   handle_resume do
+    Log.v "SENSORREG", "registered accel"
     @sensors.registerListener self, @accelerometer, SensorManager::SENSOR_DELAY_UI if @accelerometer
   end
 
