4916eb8f585711b925e27ba53f1bc2a8e828bcb7
Daniel Jackoway <danjdel@gmail.com>
2010-11-11 15:06:07 -0600

    get rid of logging, change the title

--- assets/scripts/accelerate_activity.rb
+++ assets/scripts/accelerate_activity.rb
@@ -9,7 +9,7 @@ java_import "android.graphics.Color"
 java_import "android.hardware.Sensor"
 
 $activity.when_launched do |bundle|
-  setTitle 'This is the Title'
+  setTitle 'Shake to Change Color'
 
   @sensors = getSystemService Context::SENSOR_SERVICE
   accelerometers = @sensors.getSensorList(Sensor::TYPE_ACCELEROMETER)
@@ -24,9 +24,6 @@ $activity.when_launched do |bundle|
   handle_sensor_changed do |sensor_event|
     if sensor_event.sensor.getType == Sensor::TYPE_ACCELEROMETER
       vals = sensor_event.values
-      Log.v "Values", vals[0].to_s
-      Log.v "Values", vals[1].to_s
-      Log.v "Values", vals[2].to_s
       if Math.sqrt(vals[0] ** 2 + vals[1] ** 2 + vals[2] ** 2) > 12
         getWindow.setBackgroundDrawable ColorDrawable.new(Color.rgb(rand(255), rand(255), rand(255)))
       end
@@ -34,12 +31,10 @@ $activity.when_launched do |bundle|
   end
 
   handle_pause do
-    Log.v "SENSORREG", "unregistered accel"
     @sensors.unregisterListener self, @accelerometer
   end
 
   handle_resume do
-    Log.v "SENSORREG", "registered accel"
     @sensors.registerListener self, @accelerometer, SensorManager::SENSOR_DELAY_UI if @accelerometer
   end
 end
